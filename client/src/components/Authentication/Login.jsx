38fb48e5ca975f999850484edf45feab40dbb117
simonmihalynagy <nagysimonmihaly@gmail.com>
2021-12-06 11:37:00 +0100

    clean up unsused files/ introduce react-hook-form to app

--- client/src/components/Authentication/Login.jsx
+++ client/src/components/Authentication/Login.jsx
@@ -1,28 +1,24 @@
 import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import axios from "axios";
+import { useForm } from "react-hook-form";
+
+const buttonStyle =
+  "hover:-translate-y-0.5 hover:bg-purple-400 transform py-6 px-6 font-medium rounded-xl text-xl bg-purple-600 text-white mt-5 border-2";
 
 export default function Login(props) {
-  const [credentials, setCredentials] = useState({
-    username: "",
-    password: "",
-  });
-
+  const { register, handleSubmit } = useForm();
   const [loginError, setLoginError] = useState("");
 
-  const inputChangeHangler = (event) => {
-    const value = event.target.value;
-    setCredentials({
-      ...credentials,
-      [event.target.name]: value,
-    });
-  };
+  const onSubmit = (data) => {
+    // const credentials = {
+    //   username: data.username,
+    //   password: data.password,
+    // };
 
-  const submitHandler = (event) => {
-    event.preventDefault();
-
+    // console.log(credentials);
     axios
-      .post("/auth/login", credentials)
+      .post("/auth/login", data)
       .then((response) => {
         if (response.data.user) {
           console.log(response.data);
@@ -30,13 +26,6 @@ export default function Login(props) {
         } else {
           setLoginError(response.data.message);
         }
-
-        // response.data.user
-        //   ? props.onLogin(response.data.user)
-        //   : (setLoginFailed(true),
-        //     alert(
-        //       "the password or/and username you entered are invalid, please try again"
-        //     ));
       })
       .catch((error) => {
         console.log(error);
@@ -49,38 +38,30 @@ export default function Login(props) {
         <h1 className="flex content-center text-2xl mt-10 mb-10">Please Log-In!</h1>
         {loginError && <div style={{ color: "red" }}>{loginError}</div>}
 
-        <form className="flex flex-col items-center" onSubmit={submitHandler}>
+        <form className="flex flex-col items-center" onSubmit={handleSubmit(onSubmit)}>
           <label className="text-xl text-gray-600"> Username</label>
           <input
             className="mb-6 shadow-xl rounded-lg focus:outline-purple-500"
-            onChange={inputChangeHangler}
             type="text"
             name="username"
-            value={credentials.username}
             required
+            {...register("username")}
           />
           <label className="text-xl text-gray-600">Password</label>
           <input
+            {...register("password")}
             className="mb-6 shadow-xl rounded-lg focus:outline-purple-500"
-            onChange={inputChangeHangler}
             type="password"
-            name="password"
-            value={credentials.password}
             required
           />
 
-          <button
-            className="hover:-translate-y-0.5 hover:bg-purple-400 transform py-6 px-9 font-medium rounded-xl text-xl bg-purple-600 text-white mt-5 border-2"
-            type="submit"
-          >
+          <button className={buttonStyle} type="submit">
             Log-in
           </button>
         </form>
         <p className="mt-10 text-xl text-gray-600">Or sign-up first:</p>
         <Link to="/signup">
-          <button className="hover:-translate-y-0.5 hover:bg-purple-400 transform py-6 px-6 font-medium rounded-xl text-xl bg-purple-600 text-white mt-5 border-2">
-            Sign-Up
-          </button>
+          <button className={buttonStyle}>Sign-Up</button>
         </Link>
       </div>
     </div>
