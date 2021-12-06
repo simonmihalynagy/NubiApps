bf4f985e0a3bf81cb3e8ef2d44ccd1e7647b75fc
simonmihalynagy <nagysimonmihaly@gmail.com>
2021-12-06 11:52:50 +0100

    add hook-form to signup form| next => validation for signup via hookform

--- client/src/components/Authentication/Signup.jsx
+++ client/src/components/Authentication/Signup.jsx
@@ -1,31 +1,17 @@
-import React, { useState } from "react";
-
+import React from "react";
+import { useForm } from "react-hook-form";
 import axios from "axios";
 
+//TAILWIND
+
+const inputStyle = "mb-6 shadow-xl rounded-lg focus:outline-purple-500";
+
 export default function Signup(props) {
-  const [credentials, setCredentials] = useState({
-    username: "",
-    firstName: "",
-    lastName: "",
-    //phone: "",
-    password: "",
-    email: "",
-    type: "admin",
-  });
+  const { register, handleSubmit } = useForm();
 
-  const inputChangeHandler = (event) => {
-    const value = event.target.value;
-
-    setCredentials({
-      ...credentials,
-      [event.target.name]: value,
-    });
-  };
-
-  const submitHandler = (event) => {
-    event.preventDefault();
+  const onSubmit = (data) => {
     axios
-      .post("/auth/signup", credentials)
+      .post("/auth/signup", { ...data, type: "admin" })
       .then((response) => {
         return response.data.user
           ? (console.log(response.data), props.onSignup(response.data.user))
@@ -43,61 +29,23 @@ export default function Signup(props) {
           <h1 className="text-center pt-10 text-2xl">Please Fill-Out The Form Below, To Register!</h1>
         </div>
         <div className="pt-10">
-          <form className=" flex flex-col items-center" onSubmit={submitHandler}>
+          <form className=" flex flex-col items-center" onSubmit={handleSubmit(onSubmit)}>
             <label className="text-xl text-gray-600">First Name</label>
-            <input
-              className="mb-6 shadow-xl rounded-lg focus:outline-purple-500"
-              onChange={inputChangeHandler}
-              type="text"
-              name="firstName"
-              value={credentials.firstName}
-              required
-            />
+            <input {...register("firstName")} className={inputStyle} type="text" required />
             <label className="text-xl text-gray-600">Last Name</label>
-            <input
-              className="mb-6 shadow-xl rounded-lg focus:outline-purple-500"
-              onChange={inputChangeHandler}
-              type="text"
-              name="lastName"
-              value={credentials.lastName}
-              required
-            />
+            <input {...register("lastName")} className={inputStyle} type="text" required />
             <label className="text-xl text-gray-600">Choose A Username</label>
-            <input
-              className="mb-6 shadow-xl rounded-lg focus:outline-purple-500"
-              onChange={inputChangeHandler}
-              type="text"
-              name="username"
-              value={credentials.username}
-              required
-            />
+            <input {...register("username")} className={inputStyle} type="text" required />
             <label className="text-xl text-gray-600">Choose A Password</label>
             <input
+              {...register("password")}
               className="mb-6 shadow-xl rounded-lg focus:outline-purple-500"
-              onChange={inputChangeHandler}
               type="password"
-              name="password"
-              value={credentials.password}
               required
             />
-            {/* <label>Your Phone Number</label>
-            <input
-              className="mb-6 shadow-xl rounded-lg focus:outline-purple-500"
-              onChange={inputChangeHandler}
-              type="text"
-              name="phone"
-              value={credentials.phone}
-              required
-            /> */}
+
             <label className="text-xl text-gray-600">E-mail</label>
-            <input
-              className="mb-6 shadow-xl rounded-lg focus:outline-purple-500"
-              onChange={inputChangeHandler}
-              type="email"
-              name="email"
-              value={credentials.email}
-              required
-            />
+            <input className={inputStyle} {...register("email")} type="email" name="email" required />
 
             <button
               className="hover:-translate-y-0.5 font-medium hover:bg-purple-400 transform py-6 px-6 rounded-xl text-xl bg-purple-600 text-white mt-10 border-2"
